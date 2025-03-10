a0e7a14e25df063738de1310f7f060286d26427c
CCRami <tabib.rami@esprit.tn>
2025-03-10 22:18:29 +0100

    MVC Rest Controller pt1

--- src/main/java/com/example/firstproject/Controllers/ClientController.java
+++ src/main/java/com/example/firstproject/Controllers/ClientController.java
@@ -1,12 +1,29 @@
-package com.example.firstproject.Controllers;
+    package com.example.firstproject.Controllers;
 
-import com.example.firstproject.Services.ClientService;
-import com.example.firstproject.Services.IClientService;
-import lombok.AllArgsConstructor;
-import org.springframework.stereotype.Controller;
+    import com.example.firstproject.Entities.Client;
+    import com.example.firstproject.Services.ClientService;
+    import com.example.firstproject.Services.IClientService;
+    import lombok.AllArgsConstructor;
+    import org.springframework.http.ResponseEntity;
+    import org.springframework.stereotype.Controller;
+    import org.springframework.web.bind.annotation.*;
 
-@Controller
-@AllArgsConstructor
-public class ClientController {
-    IClientService clientService;
-}
+    import java.util.List;
+
+    @RestController
+    @AllArgsConstructor
+    @RequestMapping("/client")
+    public class ClientController {
+        IClientService clientService;
+        @GetMapping("/retrieve-all-clients")
+        public ResponseEntity<List<Client>> getAllClients() {
+            List<Client> clients = clientService.retrieveAllClients();
+            System.out.println(clients);
+            return ResponseEntity.ok(clients);
+        }
+        @PostMapping("/add-client")
+        public void addClient(@RequestBody Client c){
+            System.out.println("Received Client: " + c.getIdentifiant());
+            clientService.addClient(c);
+        }
+    }
